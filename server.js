f84552e9b34f75db6bf2caf2efb4de52f0b029a9
mani3354488 <mani3354488@gmail.com>
2021-03-23 01:07:31 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const mongoose = require('mongoose')
+const express = require('express')
+
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/test'
+const port = process.env.PORT || 3000
+
+const app = express()
+app.use(express.json())
+
+const User = mongoose.model('User', {
+    name: String, 
+    email: String,
+    surname: String
+})
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.get('/users', async(req, res) => {
+    const users = await User.find({}).limit(10)
+    res.send(users)
+})
+
+app.post('/users', async (req, res) => {
+    const user = await new User(req.body.user).save()
+    res.send(user)
+})
+
+
+const run = async () => {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    await app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}!`)
+    })
+}
+  
+run()
+
